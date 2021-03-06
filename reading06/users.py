74b9502d78334762757fcba7a5f1e5d2e7bf4c40
Peter Bui <pbui@nd.edu>
2021-03-05 20:45:19 -0500

    Reading 06: add users template

---
+++ reading06/users.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import csv
+
+# Constants
+
+PATH = '/etc/passwd'
+
+# TODO: Loop through ':' delimited data in PATH and extract the fifth field
+# (user description)
+pass
+
+# TODO: Print user descriptions in sorted order
+pass
